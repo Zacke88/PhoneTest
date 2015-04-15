2da0ca7d2b706966247b843b0fbf443dac0f0923
Zacke88 <zakrisson88@gmail.com>
2015-04-15 13:48:09 +0200

    new class

---
+++ app/src/main/java/zacke/phonetest/MyCurrentLocationListener.java
@@ -0,0 +1,26 @@
+package zacke.phonetest;
+
+import android.widget.TextView;
+import android.location.Location;
+import android.location.LocationListener;
+
+/**
+ * Created by Joakim on 2015-04-15.
+ */
+public class MyCurrentLoctionListener implements LocationListener {
+
+    public String myLocation;
+    private TextView mTextView;
+
+    MyCurrentLoctionListener(TextView tv) {
+        this.mTextView = tv;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        location.getLatitude();
+        location.getLongitude();
+
+        mTextView.setText("Latitude = " + location.getLatitude() + " Longitude = " + location.getLongitude());
+
+    }
