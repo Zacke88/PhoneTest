9d01f5a597522aca4acda7fdf15e31edd1c5091a
rs <robinschulzes@gmail.com>
2015-04-17 16:16:22 +0200

    Added edittext, size and more

--- app/src/main/java/zacke/phonetest/MainActivity.java
+++ app/src/main/java/zacke/phonetest/MainActivity.java
@@ -1,23 +1,33 @@
 package zacke.phonetest;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.InputType;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -45,7 +55,6 @@ public class MainActivity extends ActionBarActivity {
         TextView tv = (TextView)findViewById(R.id.textView);
 
 
-
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
         Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
@@ -59,31 +68,83 @@ public class MainActivity extends ActionBarActivity {
 
 
         AlertDialog.Builder alert = new AlertDialog.Builder(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+      // final AlertDialog d = alert.setView(new View(this)).create();
+        // (That new View is just there to have something inside the dialog that can grow big enough to cover the whole screen.)
+
+//        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+//        lp.copyFrom(d.getWindow().getAttributes());
+//        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+//        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+//        d.show();
+//        d.getWindow().setAttributes(lp);
 
         alert.setTitle("Emergency number");
-        //alert.setMessage("Message");
 
+        //alert.setMessage("Message");
+//        AlertDialog ad = alert.create();
+//        ad.setView(v,0,0,0,0);
 // Set an EditText view to get user input
         final EditText input = new EditText(this);
+        input.setId(R.id.emdialog);
         input.setInputType(InputType.TYPE_CLASS_NUMBER);
-        //input.setRawInputType(Configuration.KEYBOARD_12KEY);
         alert.setView(input);
-        input.requestFocus();
+        input.setTextColor(Color.WHITE);
+        input.setTextSize(TypedValue.COMPLEX_UNIT_SP,160);
+        input.setSingleLine(true);
+        int maxLength = 3;
+
+        input.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        final AlertDialog ad = alert.create();
+        input.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Toast.makeText(MainActivity.this, "Got IME Done", Toast.LENGTH_SHORT).show();
+                ad.getButton(DialogInterface.BUTTON_POSITIVE).setPressed(true);
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+
+                ad.dismiss();
+                return true;
+            }
+        });
+
+
+
+
+
 
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
+
                 Editable value = input.getText();
                 // Do something with value!
+                //Lägg in close keypad
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                //d.cancel();
             }
         });
 
         alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 // Canceled.
+                //Lägg in close keypad
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
+                //d.cancel();
             }
         });
 
-        alert.show();
+
+        ad.show();
+
+
 
 
 
@@ -111,5 +172,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void OKButton(String s)  {
+
+        TextView tv = (TextView)findViewById(R.id.textView2);
+
+        //String texten = " PUSHED THE BUTTON ";
+        tv.setText(s);
+
+
+        Log.e("Tag", "Tjennnee");
+
     }
 }
